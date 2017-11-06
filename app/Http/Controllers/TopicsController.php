19753b77ebea4ae617cf761f0d324f6524f0eb94
HimmaYiJijiang <27532431@qq.com>
2017-11-06 16:23:35 +0800

    Topic index page

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -2,23 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Topic;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\TopicRequest;
 
 class TopicsController extends Controller
 {
-    protected $topic;
 
     /**
      * TopicsController constructor.
-     * @param $topic
      */
-    public function __construct($topic)
+    public function __construct()
     {
-        $this->topic = $topic;
+        $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
     public function index()
     {
+        $topics = Topic::with('user','category')->paginate(30);
         return view('topics.index', compact('topics'));
+    }
+
+    public function show(Topic $topic)
+    {
+        return view('topics.show', compact('topic'));
+    }
+
+    public function create(Topic $topic)
+    {
+        return view('topics.create_and_edit', compact('topic'));
+    }
+
+    public function store(TopicRequest $request)
+    {
+        $topic = Topic::create($request->all());
+        return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+    }
+
+    public function edit(topic $topic)
+    {
+        $this->authorize('update', $topic);
+        return view('topics.create_and_edit', compact('project'));
+    }
+
+    public function update(TopicRequest $request, Topic $topic)
+    {
+        $this->authorize('update', $topic);
+        $topic->update($request->all());
+
+        return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+    }
+
+    public function destroy(Topic $topic)
+    {
+        $this->authorize('destroy', $topic);
+        $topic->delete();
+
+        return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
     }
 }
