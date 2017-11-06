ffcdaef8509d9ec6f1b36c1c12dcf79c9b9dddd7
HimmaYiJijiang <27532431@qq.com>
2017-11-06 15:56:42 +0800

    add topic list

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class TopicsController extends Controller
 {
-    //
+    protected $topic;
+
+    /**
+     * TopicsController constructor.
+     * @param $topic
+     */
+    public function __construct($topic)
+    {
+        $this->topic = $topic;
+    }
+
+    public function index()
+    {
+        return view('topics.index', compact('topics'));
+    }
 }
--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,7 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Topic
+ * @package App\Models
+ */
 class Topic extends Model
 {
-    //
+    /**
+     * @var array
+     */
+    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
