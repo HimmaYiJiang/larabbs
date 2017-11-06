92ff713a1637296a904c4172334f4f5f7e84d558
HimmaYiJijiang <27532431@qq.com>
2017-11-06 16:43:40 +0800

    Topic order

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ Auth::routes();
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 Route::resource('topics', 'TopicsController');
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
