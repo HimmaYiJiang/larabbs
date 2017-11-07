dc9b9a90c45fc11c923cd2f9aaef928693d8e0bf
HimmaYiJijiang <27532431@qq.com>
2017-11-07 15:23:11 +0800

    Users can edit they own topic

--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -2,9 +2,14 @@
 
 namespace App\Policies;
 
+use App\Models\Topic;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
+/**
+ * Class TopicPolicy
+ * @package App\Policies
+ */
 class TopicPolicy
 {
     use HandlesAuthorization;
@@ -17,5 +22,15 @@ class TopicPolicy
     public function __construct()
     {
         //
+    }
+
+    /**
+     * @param User $currentUser
+     * @param Topic $topic
+     * @return bool
+     */
+    public function update(User $currentUser, Topic $topic)
+    {
+        return $currentUser->id === $topic->user_id;
     }
 }
