e87fc2e3378c00a42fb0faa08ff8b6bb2651bcad
HimmaYiJijiang <27532431@qq.com>
2017-11-08 16:05:57 +0800

    Notify author when topic replied

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Auth;
 
 /**
  * Class User
@@ -11,7 +12,22 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
  */
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable {
+        notify as protected laravelNotify;
+    }
+
+    /**
+     * @param $instance
+     */
+    public function notify($instance)
+    {
+        // 如果要通知的人是当前用户，就不必通知了！
+        if ($this->id == Auth::id()) {
+            return;
+        }
+        $this->increment('notification_count');
+        $this->laravelNotify($instance);
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -54,5 +70,15 @@ class User extends Authenticatable
     public function replies()
     {
         return $this->hasMany(Reply::class);
+    }
+
+    /**
+     *
+     */
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
     }
 }
--- resources/views/topics/_reply_box.blade.php
+++ resources/views/topics/_reply_box.blade.php
@@ -1,4 +1,7 @@
 <div class="reply-box">
+
+    @include('common.error')
+
     <form action="{{ route('replies.store') }}" method="POST" accept-charset="UTF-8">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <input type="hidden" name="topic_id" value="{{ $topic->id }}">
