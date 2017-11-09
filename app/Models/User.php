5cab17e8b22e6c8db377e19a2f0b6f0c02eb5bae
HimmaYiJijiang <27532431@qq.com>
2017-11-09 17:10:33 +0800

    Keep track of user last visited time

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,7 @@ use Spatie\Permission\Traits\HasRoles;
  */
 class User extends Authenticatable
 {
+    use Traits\LastActivedAtHelper;
     use Traits\ActiveUserHelper;
     use HasRoles;
     use Notifiable {
